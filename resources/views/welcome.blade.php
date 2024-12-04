21a3daa2d64545943f0de96adcc98fc5f6f889d6
ZakariaGhannam <93552159+YoussefGhannam@users.noreply.github.com>
2024-12-04 11:43:48 +0100

    add yml file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>WYDAD</title>
+        <title>WcvYDAD</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
